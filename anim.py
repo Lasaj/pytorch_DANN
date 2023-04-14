746d7f2024d7445fb2be143f1bfa9bb60b38e42f
Lasaj <rick.wainwright2@gmail.com>
2023-04-14 10:42:07 +1000

    fix loading features

--- anim.py
+++ anim.py
@@ -81,12 +81,12 @@ def perform_tsne(device, features, imgs, labels, domains, save_name):
     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=3000)
 
     last_features = features[-1]
-    encoder.load_state_dict(torch.load(last_features, map_location=device))
+    encoder.load_state_dict(torch.load(str(last_features)))
     last_features = encoder(imgs)
     tsne_transform = tsne.fit(last_features.detach().cpu().numpy())
 
     for epoch, epoch_features in enumerate(features):
-        encoder.load_state_dict(torch.load(last_features, map_location=device))
+        encoder.load_state_dict(torch.load(str(last_features)))
 
         print(f"TSNE epoch {epoch}")
         combined_feature = encoder(imgs)  # combined_feature : 1024,2352
