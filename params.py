45377eb5418fe01c5b97bb5141e245999218ab0a
Lasaj <rick.wainwright2@gmail.com>
2023-05-31 08:49:18 +1000

    old_covid training

--- params.py
+++ params.py
@@ -1,12 +1,16 @@
-batch_size = 16
+# Change experiment parameters here #
+batch_size = 32
 epochs = 100
 num_workers = 2
 discriminator_loss = 'crossentropy'  # Available: 'crossentropy', 'kl'
 encoder_type = 'inceptionv3'  # Available: 'inceptionv3', 'extractor'
 experiment_type = 'dann'  # Available: 'dann', 'source_only'
-data_type = 'covidx'  # Available: 'mnist', 'covidx', 'old_covidx'
+data_type = 'old_covidx'  # Available: 'mnist', 'covidx', 'old_covidx'
+
+# Change data parameters here #
 covidx_dir = './covid_x/'
 old_covidx_dir = './old_covid_x/'
 
-visualise_batches = 30  # n * batch_size for TSNE sample (16 works for MNIST)
+# Change visualisation parameters here #
+visualise_batches = 30  # n * batch_size for TSNE sample (might need to change batch_size to 16)
 visualise_data_set = "train"  # Available: 'train', 'test'
