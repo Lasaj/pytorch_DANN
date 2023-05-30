237b8ec1d4c738e256061fd0186bfd33051dc2e1
Lasaj <rick.wainwright2@gmail.com>
2023-05-31 08:44:56 +1000

    fix out_features for old_covidx

--- main.py
+++ main.py
@@ -19,16 +19,19 @@ def main():
     if params.data_type == 'mnist':
         source_train_loader, _, source_test_loader = mnist.get_source_dataloaders(encoder_type)
         target_train_loader, _, target_test_loader = mnistm.get_test_dataloaders(encoder_type)
+        out_features = 10
     elif params.data_type == 'covidx':
         source_train_loader, target_train_loader, source_test_loader, target_test_loader = covid_x.get_data()
+        out_features = 2
     else:
         source_train_loader, target_train_loader, source_test_loader, target_test_loader = old_covid_x.get_data()
+        out_features = 3
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'Running on {device}')
     if encoder_type == 'inceptionv3':
         encoder = model.get_iv3().to(device)
-        classifier = model.Classifier(in_features=2048, out_features=2).to(device)
+        classifier = model.Classifier(in_features=2048, out_features=out_features).to(device)
         discriminator = model.Discriminator(in_features=2048).to(device)
     else:
         encoder = model.Extractor().to(device)
